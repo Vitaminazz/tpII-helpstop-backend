db8503a3ef9902d14a2b0ffffc499ef1a9c271b9
Vitaminazz <vitamina.twitu@gmail.com>
2025-02-24 12:34:17 -0300

    Criação básica dos domínios de entidades

---
+++ HelpApp/HelpApp.Domain/Entities/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpApp.Domain.Entities
+{
+    internal class Products
+    {
+        #region Atributos
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        #endregion
+
+        #region Constructors
+        public Products(string name, string description, decimal price, int stock, string image)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+        public Category Category { get; set; }
+        #endregion
+    }
+}
---
+++ HelpApp/HelpApp.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace HelpApp.Domain.Validation
+{
+    internal class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string error) : base(error) { }
+        public static void when(bool haserror, string error)
+        {
+            if (haserror)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+        }
+    }
+}
