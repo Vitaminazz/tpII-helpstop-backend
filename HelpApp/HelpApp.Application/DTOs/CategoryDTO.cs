b7438a0b0f9c768d1319088aa8bcec0d873e4509
Vitaminazz <vitamina.twitu@gmail.com>
2025-05-12 12:49:45 -0300

    Criação DTOs, Mapping e Services

---
+++ HelpApp/HelpApp.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpApp.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        [Required(ErrorMessage = "Invalid name, too short, minimum 3 characters.")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        }
+    }
---
+++ HelpApp/HelpApp.Application/Interfaces/ICategoryServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HelpApp.Application.DTOs;
+
+    namespace HelpApp.Application.Interfaces
+{
+    internal interface ICategoryServices
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Remove(int? id);
+    }
+}
